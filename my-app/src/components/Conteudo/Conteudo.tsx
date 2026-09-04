fc3ba9a9a42da8f99f30587890034aa12647f375
Gizetto61 <gigiozetto@gmail.com>
2026-09-04 09:38:45 -0300

    feat: Exemplificando status com useState.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -3,15 +3,31 @@ import imgQuadrada from "../../img/quadrado.png";
 
 export default function Conteudo(){
 
-    const[mostraSecao, setMostraSecao] = useState(false);
+    let nome:string|null = "Giovanni";
 
-    const alternaSecao = () =>{
-        setMostraSecao(!mostraSecao);
-        console.log(mostraSecao);
+    const [nomeState, setNomeState] = useState<string | null>("Joel");
+
+    function alteraNome(){
+        nome = prompt("Digite o novo nome: ");
+        console.log("Nome digitado: ", nome);
+    }
+
+    function alteraState(){
+        const nome:string|null = prompt("Digite seu nome: ");
+        setNomeState((nomeStateAnterior) => nomeStateAnterior = nome);
+        console.log("Nome digitado: ", nomeState);
     }
 
     return(
         <main>
+            <div>
+                <p> Nome: {nome} </p>
+                <button onClick={alteraNome}>Nome = {nome}</button>
+            </div>
+            <div>
+                <p>Nome State: {nomeState}</p>
+                <button onClick={alteraState}>Nome = {nomeState}</button>
+            </div>
             <section>
                 <h2>Conteúdo Básico</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum id doloremque sint esse odio tempore ipsam, quas debitis corrupti cupiditate necessitatibus unde ratione nam? Quos optio eos rem qui placeat.</p>
@@ -39,7 +55,6 @@ export default function Conteudo(){
                 <h2>Especial</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et quos maxime temporibus eveniet nesciunt at aliquam ipsum qui nobis, beatae facere cumque debitis. Modi quod beatae nemo iure optio nam esse corrupti dolorum, nulla voluptatum ad asperiores exercitationem quis assumenda suscipit quo? Ratione deleniti ipsam qui quibusdam iste sed ea.</p>
             </section>
-            <button onClick={alternaSecao}>{mostraSecao ? "Esconder" : "Mostrar"}</button>
         </main>
     );
 }
