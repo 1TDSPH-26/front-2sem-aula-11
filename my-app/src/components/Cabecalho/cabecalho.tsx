e1383408f06c43caedd461d2fcdb3f49a7595e51
Gabrieo367 <gabrieo367@gmail.com>
2026-09-04 09:40:47 -0300

    feat: Exemplicando estados usando user states

---
+++ my-app/src/components/Cabecalho/cabecalho.tsx
@@ -0,0 +1,15 @@
+
+export default function Cabecalho() {
+    return (
+        <header>
+            <h1>MUTILADOR NOTURNO</h1>
+            <nav>
+                <ul>
+                    <li>Jonas Aguiar</li>
+                    <li>Machado do Mutilador</li>
+                    <li>Alice Lucina</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
