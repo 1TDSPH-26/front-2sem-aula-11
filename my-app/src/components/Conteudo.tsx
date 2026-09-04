97518481abe3110c8aeca407c8f2312aa5c1e249
Euthiaguera <Piedadethiago7@gmail.com>
2026-09-04 09:38:05 -0300

    feat: Exemplificando estados com state

--- my-app/src/components/Conteudo.tsx
+++ my-app/src/components/Conteudo.tsx
@@ -1,45 +1,74 @@
+
 import { useState } from "react";
 import imgQuadrada from "../img/quadrado.png";
 
 export default function Conteudo() {
-    const[mostraSecao, setMostraSecao] = useState(false);
 
-   const alternaSecao = () => {
-  setMostraSecao(!mostraSecao);
-  console.log(mostraSecao);
-}
+  let nome: string | null = "Thiago";
+
+  const [nomeState, setNomeState] = useState<String|null>("Joel")
+
+  function alteraNome() {
+    nome = prompt("Digite o novo nome : ");
+    console.log("Nome digitado: ", nome);
+  }
+
+  function alteraState(){
+    const nome: string | null  = prompt("digite seu nome:")
+    setNomeState((nomeStateAnterior)=> nomeStateAnterior = nome);
+    console.log("Nome digitado: ", nomeState);
+
+
+  }
 
   return (
     <main>
-        <section>
-            <h2>Contéudo da minha aplicação</h2>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque modi magni, veniam, accusamus dolorem veritatis quas dolores repellendus libero ratione fuga, ad nobis? Rem doloribus odio deleniti repellendus! Totam, doloremque.</p>
-        </section>
-        <section>
-            <h2>Exemplo de imagens</h2>
-            {/*Esta imagem tem referência exterta*/}
-            <figure>
-            <img src="https://placehold.co/600x400/fff333/FFFFFF/png" 
-            alt="Exemplo de imagem" />
-            <figcaption>Bloco de imagem 600 x 400</figcaption>
-            </figure>
-            {/*Esta imagem tem referência interna na /src*/}
-            <figure>
-            <img src={imgQuadrada} alt="Exemplo de imagem" />
-            <figcaption>Bloco de imagem 600 x 400</figcaption>
-            </figure>
-              {/*Esta imagem tem referência interna na /public*/}
-            <figure>
-            <img src="/quadrado.png" alt="Exemplo de imagem" />
-            <figcaption>Bloco de imagem 600 x 400</figcaption>
-            </figure>
-        </section>
+      <div>
+        <p>Nome : {nome}</p>
+        <button onClick={alteraNome}>Nome = {nome}</button>
+      </div>
+      <div>
+        <p>Nome State: { nomeState}</p>
+        <button onClick={alteraState}>Nome = {nomeState}</button>
+      </div>
+      <section>
+        <h2>Conteúdo Básico</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          soluta sed vero reiciendis enim nam, aut perferendis, sit velit
+          ratione placeat, distinctio eius rerum maxime id perspiciatis
+          repudiandae non! Tenetur.
+        </p>
+      </section>
+      <section>
+        <h2>Exemplo de Imagens</h2>
+        {/* Esta imagem tem referência externa */}
+        <figure>
+          <img
+            src="https://placehold.co/600x400/fff333/ffffff/png"
+            alt="Exemplo de imagem"
+          />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
+        </figure>
 
-        <section>
-            <h2>Especial</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam dolor, animi quisquam quidem illo eaque. Ullam expedita ad dolorum animi porro. Mollitia omnis necessitatibus hic est incidunt aliquid placeat delectus dolores numquam corporis eveniet soluta qui corrupti quidem, dolorum ab aut, veniam tempora. Laborum suscipit sequi eius sit porro soluta?</p>
-        </section>
-        <button onClick={alternaSecao}>{mostraSecao ? "ESCONDER" : "MOSTRAR"}</button>
+        {/* Esta imagem tem referência interna na /src */}
+        <figure>
+          <img src={imgQuadrada} alt="Exemplo de imagem" />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
+        </figure>
+
+        {/* Esta imagem tem referência interna na public */}
+        <figure>
+          <img src="/quadrado.png" alt="Exemplo de imagem" />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
+        </figure>
+      </section>
+
+      <section >
+        <h2>Especial</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, vitae recusandae? Inventore impedit earum nisi reiciendis provident autem cum aliquam natus excepturi ut distinctio, laborum veritatis, veniam fugiat exercitationem quam iusto accusantium culpa reprehenderit quas quibusdam. Nulla, quibusdam cumque? Quibusdam dolorum dolore deserunt. Assumenda eos voluptatibus dolores eaque fugit quod?</p>
+      </section>
+
     </main>
-  )
+  );
 }
