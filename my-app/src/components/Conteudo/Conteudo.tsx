e1c6756ab87257db1e798fefb49859ad9fcb1525
felipelima2005 <felipelimaoliveira2008@gmail.com>
2026-09-04 09:37:58 -0300

    feat: Exemplo dos estados com useState

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,19 +1,34 @@
+
 import { useState } from "react";
 import imgQuadrada from "../../img/png.png";
 
 export default function Conteudo(){
 
+    let nome:string|null = "Felipe";
 
-        const[mostraSecao, setMostraSecao] = useState(false);
+    const [nomeState, setNomeState] = useState<string | null>("Joel");
 
-        const alternaSecao = () =>{
-
-        setMostraSecao(!mostraSecao);
-        console.log(mostraSecao);
+    function alteraNome(){
+        nome = prompt("Digite o novo nome: ");
+        console.log("nome digitado: ", nome);
     }
-    
+
+    function alteraState(){
+        const nome:string | null = prompt("Digite seu nome: ")
+        setNomeState((nomeStateAnterior) => nomeStateAnterior = nome);
+        console.log("Nome digitado: ",nomeState);
+    }
+
     return(
         <main>
+            <div>
+                <p>Nome : {nome}</p>
+                <button onClick={alteraNome}>Nome = {nome}</button>
+            </div>
+            <div>
+                <p>Nome State: {nomeState}</p>
+                <button onClick={alteraState}>Nome = {nomeState}</button>
+            </div>
             <section>
                 <h2>conteudo basico</h2>
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam eaque ipsum voluptate voluptatibus minima animi dolores accusamus nulla laborum asperiores ratione fugit molestias dignissimos vero expedita odio, eum dolore voluptas.</p>
@@ -45,7 +60,7 @@ export default function Conteudo(){
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis tempore distinctio enim debitis quod quaerat cupiditate, optio eaque obcaecati nihil excepturi quam consequuntur, magnam beatae delectus. Doloribus placeat, recusandae reiciendis deleniti sequi est dicta doloremque eum nihil earum vero sed libero qui nesciunt dolorem minus alias nostrum hic quisquam laboriosam!</p>
 
             </section>
-            <button onClick={alternaSecao}>{mostraSecao ? "ESCONDER": "MOSTRAR"}</button>
+            
         </main>
     )
 }
