776faeb7bbe5739af3a5b80dd45e769afcf7bd47
LuigiT2703 <luiginhotoca@gmail.com>
2026-09-04 09:38:07 -0300

    feat : colocando botoes dos nomes

--- my-app/src/components/Conteudo.tsx
+++ my-app/src/components/Conteudo.tsx
@@ -1,29 +1,53 @@
-import quadrado from '../img/quadrado.png'
 import { useState } from 'react';
-export default function Conteudo() {
-  const[mostraSecao, setMostraSecao] = useState(false);
+import quadrado from '../img/quadrado.png';
 
-  const alternaSecao = () => {
-    setMostraSecao(!mostraSecao);
-    console.log(mostraSecao);
-  };
+export default function Conteudo() {
+
+  let nome: string | null = "PEDROSALVATORE";
+  const [nomeState, setNomeState] = useState<string>("PEPE");
+
+  function alteraNome() {
+    nome = prompt("Digite o nome: ");
+    console.log("Nome digitado: ", nome);
+  }
+
+  function alteraState() {
+    setNomeState(prompt("Digite seu nome: ") || "");
+    console.log("Nome digitado:", nomeState);
+  }
 
   return (
     <main>
-      <h2>Conteúdo</h2>
+      <div>
+        <p>Nome : {nome}</p>
+
+        <button onClick={alteraNome}>
+          Nome = {nome}
+        </button>
+
+        <p>Nome State: {nomeState}</p>
+
+        <button onClick={alteraState}>
+          Alterar nome
+        </button>
+      </div>
+
       <section>
         <h2>Exemplo imagens</h2>
+
         <figure>
-          <img src="https://placehold.co/600x400/1c1c1c1/FFFFFF/png" alt="Imagem de exemplo" />
+          <img
+            src="https://placehold.co/600x400/1c1c1c/FFFFFF/png"
+            alt="Imagem de exemplo"
+          />
           <figcaption>Figura 1: Imagem de exemplo</figcaption>
         </figure>
+
         <figure>
           <img src={quadrado} alt="Imagem de exemplo" />
-          <figcaption>Figura 1: Imagem de exemplo</figcaption>
+          <figcaption>Figura 2: Imagem de exemplo</figcaption>
         </figure>
       </section>
-      <button onClick={alternaSecao}>{mostraSecao ? 'Esconder secao' : 'Mostrar secao'}</button>
     </main>
-  )
+  );
 }
-
