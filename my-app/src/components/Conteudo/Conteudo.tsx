f2c81337b27f226e6feb0c75c7e0ed95acab4f09
henriquebonachela <carabantecarvalho@gmail.com>
2026-09-04 09:40:02 -0300

    feat: Exemplificando e alterando estados com o UseState.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,54 +1,72 @@
-import { useState } from "react"
+
+import { useState } from "react";
 import imgQuadrada from "../../img/quadrado.png";
 
 export default function Conteudo() {
 
-        const[mostraSecao, setMostraSecao] = useState(false);
- 
-      const alternaSecao = () =>{
- 
-        setMostraSecao(!mostraSecao);
-        console.log(mostraSecao);
-      }
+  let nome: string | null = "Alê";
 
+  const [nomeState, setNomeState] = useState<string | null>("Joel");
+
+  function alteraNome() {
+    nome = prompt("Digite o novo nome : ");
+    console.log("Nome digitado: ", nome);
+  }
+
+  function alteraState() {
+    const nome: string | null = prompt("digite seu nome:")
+    setNomeState((nomeStateAnterior) => nomeStateAnterior = nome);
+    console.log("Nome digitado: ", nomeState);
+  }
 
   return (
     <main>
+      <div>
+        <p>Nome : {nome}</p>
+        <button onClick={alteraNome}>Nome = {nome}</button>
+      </div>
+      <div>
+        <p>Nome State: {nomeState}</p>
+        <button onClick={alteraState}>Nome = {nomeState}</button>
+      </div>
       <section>
         <h2>Conteúdo Básico</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Dignissimos soluta sed vero reiciendis enim nam, aut perferendis,
-          sit velit ratione placeat, distinctio eius rerum maxime id
-          perspiciatis repudiandae non! Tenetur.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          soluta sed vero reiciendis enim nam, aut perferendis, sit velit
+          ratione placeat, distinctio eius rerum maxime id perspiciatis
+          repudiandae non! Tenetur.
         </p>
       </section>
       <section>
         <h2>Exemplo de Imagens</h2>
-        {/*Esta imagem tem referência externa */}
+        {/* Esta imagem tem referência externa */}
         <figure>
-            <img src="https://placehold.co/400x400/c1c1c1/000000/png" 
-            alt="Exemplo de Imagem" />
-            <figcaption>Bloco de Imagem 400x400</figcaption>
+          <img
+            src="https://placehold.co/400x400/c1c1c1/000000/png"
+            alt="Exemplo de imagem"
+          />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
         </figure>
 
         {/* Esta imagem tem referência interna na /src */}
         <figure>
-            <img src= {imgQuadrada} alt="Exemplo de Imagem" />
-            <figcaption>Bloco de imagem 400 x 400</figcaption>
+          <img src={imgQuadrada} alt="Exemplo de imagem" />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
         </figure>
 
         {/* Esta imagem tem referência interna na public */}
         <figure>
-            <img src="/img/quadrado.png" alt="Exemplo de Imagem" />
+          <img src="/img/quadrado.png" alt="Exemplo de imagem" />
+          <figcaption>Bloco de Imagem 400 x 400</figcaption>
         </figure>
       </section>
 
-      <section>
+      <section >
         <h2>Especial</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere excepturi laboriosam laudantium culpa asperiores itaque odit maxime officiis, hic ipsa aliquam tempore provident neque quo voluptatum corporis dolorum est unde pariatur. Quae quaerat eaque sequi id veniam distinctio sapiente quas. Praesentium quisquam deserunt enim? Molestiae harum est obcaecati sed quis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, vitae recusandae? Inventore impedit earum nisi reiciendis provident autem cum aliquam natus excepturi ut distinctio, laborum veritatis, veniam fugiat exercitationem quam iusto accusantium culpa reprehenderit quas quibusdam. Nulla, quibusdam cumque? Quibusdam dolorum dolore deserunt. Assumenda eos voluptatibus dolores eaque fugit quod?</p>
       </section>
-      <button onClick={alternaSecao}>{mostraSecao ? "ESCONDER" : "MOSTRAR"}</button>
+
     </main>
   );
 }
